59cf620755bb666e4f79461ee08d9bc2f24faa28
Mohammed Zohil PK <44064613+zoypk@users.noreply.github.com>
2026-04-14 18:11:09 +0530

    Add: non-overlapping-intervals - submission-3.py

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-3.py
@@ -0,0 +1,80 @@
+#
+# @lc app=leetcode id= lang=python3
+#
+# [435] Non-overlapping Intervals
+#
+# https://leetcode.com/problems/non-overlapping-intervals/description/
+#
+# algorithms
+# Medium (56.85%)
+# Likes:    9138
+# Dislikes: 260
+# Total Accepted:    999.9K
+# Total Submissions: 1.8M
+# Testcase Example:  '[[1,2],[2,3],[3,4],[1,3]]'
+#
+# Given an array of intervals intervals where intervals[i] = [starti, endi],
+# return the minimum number of intervals you need to remove to make the rest of
+# the intervals non-overlapping.
+#
+# Note that intervals which only touch at a point are non-overlapping. For
+# example, [1, 2] and [2, 3] are non-overlapping.
+#
+#
+# Example 1:
+#
+#
+# Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+# Output: 1
+# Explanation: [1,3] can be removed and the rest of the intervals are
+# non-overlapping.
+#
+#
+# Example 2:
+#
+#
+# Input: intervals = [[1,2],[1,2],[1,2]]
+# Output: 2
+# Explanation: You need to remove two [1,2] to make the rest of the intervals
+# non-overlapping.
+#
+#
+# Example 3:
+#
+#
+# Input: intervals = [[1,2],[2,3]]
+# Output: 0
+# Explanation: You don't need to remove any of the intervals since they're
+# already non-overlapping.
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= intervals.length <= 10^5
+# intervals[i].length == 2
+# -5 * 10^4 <= starti < endi <= 5 * 10^4
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
+        intervals.sort()
+
+        res = 0
+
+        prevEnd = intervals[0][1]
+
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                res += 1
+                prevEnd = min(prevEnd, end)
+        return res
+
+
+# @lc code=end
