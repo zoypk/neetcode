f41b98a92cac33012c9159c57cdc0f9e5cf244d8
Mohammed Zohil PK <44064613+zoypk@users.noreply.github.com>
2026-04-21 19:23:54 +0530

    Add: longest-palindromic-substring - submission-5.py

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-5.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=5 lang=python3
+#
+# [5] Longest Palindromic Substring
+#
+# https://leetcode.com/problems/longest-palindromic-substring/description/
+#
+# algorithms
+# Medium (37.48%)
+# Likes:    32656
+# Dislikes: 2014
+# Total Accepted:    4.7M
+# Total Submissions: 12.4M
+# Testcase Example:  '"babad"'
+#
+# Given a string s, return the longest palindromic substring in s.
+#
+#
+# Example 1:
+#
+#
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+#
+#
+# Example 2:
+#
+#
+# Input: s = "cbbd"
+# Output: "bb"
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= s.length <= 1000
+# s consist of only digits and English letters.
+#
+#
+#
+
+# NeetCode
+# Solution: https://neetcode.io/problems/longest-palindromic-substring/question?list=neetcode150
+# Video: https://www.youtube.com/watch?v=XYQecbcd6_c
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+
+        return res
+
+
+# @lc code=end
